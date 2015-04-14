878f3f5517aa9dce21197ad91e621d1470c9f1ed
xiaopow <xiaoyang88@gmail.com>
2015-04-14 23:45:49 +0800

    finsih admin page functions

--- JS/admin.js
+++ JS/admin.js
@@ -9,12 +9,12 @@ $(document).ready(function(){
     }
   };
 
+  // Get all quotes in quotes collection
   function getAllQuote() {
     var newRequest = new Request();
     newRequest['type'] = "GET";
     newRequest['url'] = "http://harryquotes-216807.apse1.nitrousbox.com:8080/quotes";
     newRequest["success"] = function(response){
-      console.log(response);
       $('.return-content').text('');
       if (response.length > 0) {
           $.each(response, function(index){
@@ -25,9 +25,9 @@ $(document).ready(function(){
               <button class="update btn btn-primary">Update</button> \
               <button class="delete btn btn-primary">Delete</button> \
             </div>'
-            )
+            );
           });
-        } else {
+        } else if (response.length === 1){
           $('.return-content').append('<div class="content-row col-xs-12"> \
               <div class="col-lg-2 hidden-sm hidden-xs hidden-md id">'+response["_id"]+'</div> \
               <div contentEditable="true" class="col-xs-6 col-sm-5 quote">'+response["quote"]+'</div> \
@@ -35,13 +35,55 @@ $(document).ready(function(){
               <button class="update btn btn-primary">Update</button> \
               <button class="delete btn btn-primary">Delete</button> \
             </div>'
-          )
+          );
+        } else {
+          $('.return-content').append("No results found.");
         }
     }
 
     $.ajax(newRequest);
   }
 
+  // Search quotes collection with keyword
+  function search(keyword) {
+    var newRequest = new Request();
+    newRequest['type'] = "GET";
+    newRequest['url'] = "http://harryquotes-216807.apse1.nitrousbox.com:8080/quotes/search/"+keyword; 
+    newRequest["success"] = function(response){
+      $('.return-content').text('');
+      if (response.length > 0) {
+          $.each(response, function(index){
+            $('.return-content').append('<div class="content-row col-xs-12"> \
+              <div class="col-lg-2 hidden-sm hidden-xs hidden-md id">'+response[index]["_id"]+'</div> \
+              <div contentEditable="true" class="col-xs-6 col-sm-5 quote">'+response[index]["quote"]+'</div> \
+              <div contentEditable="true" class="col-xs-2 col-sm-2 author">'+response[index]["author"]+'</div> \
+              <button class="update btn btn-primary">Update</button> \
+              <button class="delete btn btn-primary">Delete</button> \
+            </div>'
+            );
+          });
+        } else if (response.length === 1){
+          $('.return-content').append('<div class="content-row col-xs-12"> \
+              <div class="col-lg-2 hidden-sm hidden-xs hidden-md id">'+response["_id"]+'</div> \
+              <div contentEditable="true" class="col-xs-6 col-sm-5 quote">'+response["quote"]+'</div> \
+              <div contentEditable="true" class="col-xs-2 col-sm-2 author">'+response["author"]+'</div> \
+              <button class="update btn btn-primary">Update</button> \
+              <button class="delete btn btn-primary">Delete</button> \
+            </div>'
+          );
+        } else {
+          $('.return-content').append("No results found.");
+        }
+    }
+
+    $.ajax(newRequest);
+  }
+
+  $(document).on('click','#search',function(){
+    search($('#search-term').val());
+  });
+
+  // Add a quote in quotes collection
   function addQuote() {
     var newRequest = new Request();
     newRequest['type'] = "POST";
@@ -54,7 +96,6 @@ $(document).ready(function(){
     }
     newRequest["success"] = function(response){
       getAllQuote();
-      console.log(response);
       $('#new-quote').val('');
       $('#author').val('');
     }
@@ -66,17 +107,17 @@ $(document).ready(function(){
     addQuote();
   });
 
-  // update a quote
+  // update a quote in quotes collection
   $(document).on('click','.update',function(){
     var newRequest = new Request;
+    newRequest['type'] = "PUT";
+    newRequest['url'] = "http://harryquotes-216807.apse1.nitrousbox.com:8080/quotes/" + $(this).siblings('.id').text();
     newRequest['data'] = {
       'quote': {
         'quote': $(this).siblings('.quote').text(),
         'author': $(this).siblings('.author').text()
       }
     }
-    newRequest['type'] = "PUT";
-    newRequest['url'] = "http://harryquotes-216807.apse1.nitrousbox.com:8080/quotes/" + $(this).siblings('.id').text();
     newRequest["success"] = function(response){
       getAllQuote();
     }
@@ -84,7 +125,7 @@ $(document).ready(function(){
     $.ajax(newRequest);
   });
 
-  // delete a quote
+  // delete a quote in quotes collection
   $(document).on('click','.delete',function(){
     var newRequest = new Request;
     newRequest['type'] = "DELETE";
@@ -101,4 +142,79 @@ $(document).ready(function(){
   })
 
   getAllQuote();
+
+  //----------------------------------- New Quotes Tab -------------------------------------------------
+  // Get all quotes in new quotes collection
+  function getAllNewQuote() {
+    var newRequest = new Request();
+    newRequest['type'] = "GET";
+    newRequest['url'] = "http://harryquotes-216807.apse1.nitrousbox.com:8080/newQuotes";
+    newRequest["success"] = function(response){
+      $('.return-new-content').text('');
+      if (response.length > 0) {
+          $.each(response, function(index){
+            $('.return-new-content').append('<div class="content-row col-xs-12"> \
+              <div class="col-lg-2 hidden-sm hidden-xs hidden-md id">'+response[index]["_id"]+'</div> \
+              <div contentEditable="true" class="col-xs-6 col-sm-5 quote">'+response[index]["quote"]+'</div> \
+              <div contentEditable="true" class="col-xs-2 col-sm-2 author">'+response[index]["author"]+'</div> \
+              <button class="add btn btn-primary">Add</button> \
+              <button class="delete-new btn btn-primary">Delete</button> \
+            </div>'
+            );
+          });
+        } else if (response.length === 1){
+          $('.return-new-content').append('<div class="content-row col-xs-12"> \
+              <div class="col-lg-2 hidden-sm hidden-xs hidden-md id">'+response["_id"]+'</div> \
+              <div contentEditable="true" class="col-xs-6 col-sm-5 quote">'+response["quote"]+'</div> \
+              <div contentEditable="true" class="col-xs-2 col-sm-2 author">'+response["author"]+'</div> \
+              <button class="add btn btn-primary">Add</button> \
+              <button class="delete-new btn btn-primary">Delete</button> \
+            </div>'
+          );
+        } else {
+          $('.return-new-content').append("No results found.");
+        }
+    }
+
+    $.ajax(newRequest);
+  }
+
+  // add a new quote to quotes collection
+  $(document).on('click','.add',function(){
+    var thisA = this
+    var newRequest = new Request;
+    newRequest['type'] = "POST";
+    newRequest['url'] = "http://harryquotes-216807.apse1.nitrousbox.com:8080/quotes";
+    newRequest['data'] = {
+      'quote': {
+        'quote': $(this).siblings('.quote').text(),
+        'author': $(this).siblings('.author').text()
+      }
+    }
+    newRequest["success"] = function(response){
+      getAllQuote();
+      $(thisA).siblings('.delete-new').click();
+    }
+
+    $.ajax(newRequest);
+  });
+
+  // delete a new quote from newQuotes collection
+  $(document).on('click','.delete-new',function(){
+    var newRequest = new Request;
+    newRequest['type'] = "DELETE";
+    newRequest['url'] = "http://harryquotes-216807.apse1.nitrousbox.com:8080/newQuotes/" + $(this).siblings('.id').text();
+    newRequest["success"] = function(response){
+      getAllNewQuote();
+    }
+
+    $.ajax(newRequest);
+  });
+
+  $(document).on('click','#get-all-new-quotes',function(){
+    getAllNewQuote();
+  })
+
+  getAllNewQuote();
+
 })
--- JS/application.js
+++ JS/application.js
@@ -1,56 +1,4 @@
-// $(document).ready(function(){
-  // var quotes = [
-  //   "Nothing is Ever Random. - Harry Chen 2015",
-  //   "I need beaver - Harry Chen 26/03/2015",
-  //   "Demo or Die - Harry",
-  //   "I need Bieber - Harry Chen",
-  //   "I've never heard of it - Harry Chen",
-  //   "You have to fork the repo and clone it in your computer - Harry",
-  //   "What did I just see? - Harry",
-  //   "Do you have a confession? - Harry",
-  //   "Don't GOX me bro! - Harry #classic",
-  //   "Yesterday is gone, but today is up for for grabs. - Harry Chen",
-  //   "Have you forked today? - Harry Chen #fakeharryquotes",
-  //   "Can I quickly interject? - MDA",
-  //   "A lot of you has been trying to get some action through clicking - HC",
-  //   "I wanna buy a lot of Fer today - HC",
-  //   "You go, TIGER! - Dom to Victoria",
-  //   "I tried. It's NEVER going to work for me - Ken",
-  //   "I AM Harry Potter - Harry Chen #fakeharryquotes",
-  //   "This is some geeky fun - MDC",
-  //   "That's not what Jobs said - HC",
-  //   "What ? Are we gambling here? - HC",
-  //   "Oh, there's something to fork? - XLS",
-  //   "That I can do. I don't even need eval - MDC",
-  //   "Where's your javascript? - Harryscript",
-  //   "Cloning is encouraged! - HC",
-  //   "I'm going to get my fork. It's in the trash. - Xiao",
-  //   "Harry === Geeky - HC 1/4/2015",
-  //   "Xavi just blew my mind - Elia <br/> wait.. when did I blow someone? - XLS",
-  //   "I don't sweat. Ever - XLS",
-  //   "This is ridiculous what they are suggesting - Victoria referring to codeclimate",
-  //   "Strawpedo is actually pretty good - Michael",
-  //   "If you ever have a daughter.....  -Xiao..... Uhhhhhhhhhhh",
-  //   "...I'll ruin her before you can find me... - xiao",
-  //   "I KNOW WHO'S COMING TO  MY FUNERAL - Xiao",
-  //   "that's what she said - Xiao",
-  //   "why would she say that?? - Michael",
-  //   "SO much sausage.... What do we(I) even do here... The sausage has exploded.... Oh wow that's good hmmmm mmmmmm - Dom",
-  //   "I want to get real seriously deep tonight - Dom",
-  //   "it's all in the back hand - Dom, 'the beast' dunnet",
-  //   "Do you want me to hold it for you do you can do it with two hands? - Dimple",
-  //   "When programmers see an INFINITE amount of forms to access a government, they are like: GG - Harry Chen",
-  //   "You Are Being Smart Today - The DOM.",
-  //   "This was Distracting (Points to [Dom's] Balls) - HC",
-  //   "So far you've all been consuming from my backend - HC",
-  //   "I got rickroll'ed! I got rickroll'ed! - Xiao",
-  //   "WOAAH - Mia",
-  //   "Omg! Did it go to Mars? OMG! Did I go to Mars? - HC",
-  //   "I'm not EVEN joking - HC",
-  //   "WoW - HC",
-  //   "෴ - HC",
-  //   "It's kinda like a prostitution - MDC"
-  // ];
+$(document).ready(function(){
 
   var totalClick = 0;
 
@@ -108,7 +56,14 @@
   //  {quote: "It's kinda like a prostitution", author: "MDC"},
   //  {quote: "I'm Learning!", author: "Xiao"},
   //  {quote: "I would SMASH that in my face right now", author: "MDC"}
-  // ]
+  // ];
+
+  // function postQuote() {
+  //   var randomDB = Math.round(Math.random() * db.length);    
+  //   var dbQ = db[randomDB].quote;
+  //   var dbA = db[randomDB].author;
+  //   $('#quote').text(dbQ + " - " + dbA)
+  // };
 
   function Request() {
     this.type = '';
@@ -119,7 +74,7 @@
     }
   };
 
-  function postQuote() {
+  function postQuoteToHTML() {
     var newRequest = new Request();
     newRequest['type'] = "GET";
     newRequest['url'] = "http://harryquotes-216807.apse1.nitrousbox.com:8080/quotes/random";
@@ -128,25 +83,14 @@
     }
 
     $.ajax(newRequest);
-  }
+  };
     
-  // function postQuote() {
-  //   var randomDB = Math.round(Math.random() * db.length);    
-  //   var dbQ = db[randomDB].quote;
-  //   var dbA = db[randomDB].author;
-  //   $('#quote').text(dbQ + " - " + dbA)
-  // };
   
-  // function postQuote() {
-  //   var randomQuote = Math.floor(Math.random() * quotes.length);
-  //   var quote = quotes[randomQuote];
-  //   $('#quote').text(quote);
-  // };
   
   function addQuote() {
     var newRequest = new Request();
     newRequest['type'] = "POST";
-    newRequest['url'] = "http://harryquotes-216807.apse1.nitrousbox.com:8080/quotes";
+    newRequest['url'] = "http://harryquotes-216807.apse1.nitrousbox.com:8080/newQuotes";
     newRequest['data'] = {
       'quote': {
         'quote': $('#new-quote').val(),
@@ -160,7 +104,7 @@
     }
 
     $.ajax(newRequest);
-  }
+  };
 
   $(document).on('click','#submit',function(){
     if($('#password').val()==='harry'){
@@ -172,10 +116,10 @@
 
   $(document).on('click','#submit-quote',function(){
     $('.submit-form').toggleClass('dontDisplay');
-  })
+  });
 
   $(document).on('click','#astro',function(){
-    postQuote();
+    postQuoteToHTML();
     totalClick++;
 
     if (totalClick === 10) {
@@ -199,4 +143,4 @@
     $(this).attr('src','assets/astro.png')
   });
 
-// });
+});
